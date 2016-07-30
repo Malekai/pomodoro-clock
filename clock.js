53a8b2041daa815d8654c84d433d959ae85ceca7
Malekai <mmalek1421@gmail.com>
2016-07-30 19:17:11 -0400

    pomodoro's are now stored separately, so application remembers user setting instead of going back to 25 by default

--- clock.js
+++ clock.js
@@ -1,46 +1,64 @@
 //set variables that change for session
-  var sessionNum = $("#sessionNum");
-  var fullTime = sessionNum.html();
-  var timeDigits = Number(fullTime[0] + fullTime[1])
-  var breakNum = $("#breakNum");
-  var breakTime = breakNum.html();
-  var digits = Number(breakTime[0]);
+var sessionDisplay = $("#sessionDisplay");
+var sessionNum = $("#sessionNum");
+var fullTime = sessionNum.html();
+var timeDigits = Number(fullTime[0] + fullTime[1])
+var breakNum = $("#breakNum");
+var breakTime = breakNum.html();
+var digits = Number(breakTime[0]);
 
 //make + and - buttons adjust the variable up and down
-  $('#plusSession').click(function(event){
-    event.stopPropagation();
-    //bring up the start time
-    if (timeDigits < 60) {
-      timeDigits++;
-      sessionNum.html(String(timeDigits) + ":" + "00");
+$('#plusSession').click(function(event){
+  event.stopPropagation();
+  //bring up the start time
+  if (timeDigits < 60) {
+    timeDigits++;
+    sessionNum.html(String(timeDigits) + ":" + "00");
+    //make sure the display only moves if we're not on break
+    if(!$('#sessionNum').hasClass("onBreak")) {
+      sessionDisplay.html(String(timeDigits) + ":" + "00");
     }
-  });
+  }
+});
 
-  $('#minusSession').click(function(event){
-    event.stopPropagation();
-    //bring down start time
-    if (timeDigits > 1) {
-      timeDigits--;
-      sessionNum.html(String(timeDigits) + ":" + "00");
+$('#minusSession').click(function(event){
+  event.stopPropagation();
+  //bring down start time
+  if (timeDigits > 1) {
+    timeDigits--;
+    sessionNum.html(String(timeDigits) + ":" + "00");
+    //make sure the display only moves if we're not on break
+    if(!$('#sessionNum').hasClass("onBreak")) {
+      sessionDisplay.html(String(timeDigits) + ":" + "00");
     }
-  });
+  }
+});
 
-  $('#plusBreak').click(function(){
-    //bring up the start time
-    if (digits < 60) {
-      digits++;
-      breakNum.html(String(digits) + ":" + "00");
+
+
+$('#plusBreak').click(function(){
+  //bring up the start time
+  if (digits < 60) {
+    digits++;
+    breakNum.html(String(digits) + ":" + "00");
+    //make sure the display only moves if we're on break
+    if ($('#sessionNum').hasClass("onBreak")) {
+      sessionDisplay.html(String(digits) + ":" + "00");
     }
-  });
+  }
+});
 
-  $('#minusBreak').click(function(){
-    //bring down start time
-    if (digits > 1) {
-      digits--;
-      breakNum.html(String(digits) + ":" + "00");
+$('#minusBreak').click(function(){
+  //bring down start time
+  if (digits > 1) {
+    digits--;
+    breakNum.html(String(digits) + ":" + "00");
+    //make sure the display only moves if we're not on break
+    if ($('#sessionNum').hasClass("onBreak")) {
+      sessionDisplay.html(String(digits) + ":" + "00");
     }
-  });
-
+  }
+});
 
 //make timer countdown on and off when clicked
 $('.circle').click(function(){
@@ -57,7 +75,7 @@ var t = 0;
 var timeStore
 //function to initiate timer
 function timer() {
-  var myTime = sessionNum.html();
+  var myTime = sessionDisplay.html();
   //check if clock has hit 00:00 and then turn into break
   if (myTime == "00:00") {
     var audio = new Audio('Beeper.mp3');
@@ -65,16 +83,15 @@ function timer() {
     $('.circle').toggleClass('start');
     $('#sessionNum').toggleClass("onBreak");
     $('h2').toggleClass("orange");
-    $('#sessionNum').toggleClass("orange");
+    $('#sessionDisplay').toggleClass("orange");
     if ($('#sessionNum').hasClass("onBreak")) {
-      $('#sessionNum').html(String(digits) + ":" + "00");
+      $('#sessionDisplay').html(String(digits) + ":" + "00");
       $('h2').text("Break Time!");
       return;
     } //make timer go back to session time
     else if(!$('#sessionNum').hasClass("onBreak")) {
-      timeDigits = Number(fullTime[0] + fullTime[1])
-      $('#sessionNum').html(String(timeDigits) + ":" + "00");
-      $('h2').text("Session");
+      $('#sessionDisplay').html(String(timeDigits) + ":" + "00");
+      $('h2').text("Pomodoro");
       return;
     }
   }
@@ -93,7 +110,7 @@ function timer() {
   //split time up
   var ts = temp[0].split(":");
 
-  sessionNum.html(ts[1]+":"+ts[2]);
+  sessionDisplay.html(ts[1]+":"+ts[2]);
   t = setTimeout(timer, 1000);
 
 }
